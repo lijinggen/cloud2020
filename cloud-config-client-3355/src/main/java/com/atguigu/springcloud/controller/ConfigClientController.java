73e9740d3aa17413d0af22d410e0cb782eef71c9
lijinggen <13192819231q@gmail.com>
2020-08-24 16:48:07 +0800

    #

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class ConfigClientController {
-    @Value("${config.info}")
+    @Value("${config.spring}")
     private String configInfo;
 
     @GetMapping("/configInfo")
